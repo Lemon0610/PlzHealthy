13499d85b5299520efe8619bbefc67fcccd8aa6a
pizza-boy-j <jiwoo4143@gmail.com>
2026-04-03 08:50:28 +0900

    작업 준비

---
+++ app/src/main/java/com/example/plzhealth/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.plzhealth
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        replaceFragment(HealthPointFragment())
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //하단바
+        val btnHealth = findViewById<TextView>(R.id.btnHealth)
+        val btnToday = findViewById<TextView>(R.id.btnToday)
+        val btnSearch = findViewById<TextView>(R.id.btnSearch)
+        val btnMypage = findViewById<TextView>(R.id.btnMypage)
+
+        btnHealth.setOnClickListener {
+            replaceFragment(HealthPointFragment())
+        }
+
+        btnToday.setOnClickListener {
+            replaceFragment(TodayFragment())
+        }
+
+        btnSearch.setOnClickListener {
+            replaceFragment(SearchFragment())
+        }
+
+        btnMypage.setOnClickListener {
+            replaceFragment(MypageFragment())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+}
