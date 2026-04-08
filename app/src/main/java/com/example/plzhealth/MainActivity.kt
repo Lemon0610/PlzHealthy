1ceaaf391de3e8f4729ff0124a5689f9c794dcc7
pizza-boy-j <jiwoo4143@gmail.com>
2026-04-08 09:20:12 +0900

    feat: MainActivity 수정

--- app/src/main/java/com/example/plzhealth/MainActivity.kt
+++ app/src/main/java/com/example/plzhealth/MainActivity.kt
@@ -1,53 +1,1 @@
-package com.example.plzhealth
-
-import android.os.Bundle
-import android.widget.LinearLayout
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        replaceFragment(HealthPointFragment())
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        //하단바
-        val btnHealth = findViewById<LinearLayout>(R.id.btnHealth)
-        val btnToday = findViewById<LinearLayout>(R.id.btnToday)
-        val btnSearch = findViewById<LinearLayout>(R.id.btnSearch)
-        val btnMypage = findViewById<LinearLayout>(R.id.btnMypage)
-
-        btnHealth.setOnClickListener {
-            replaceFragment(HealthPointFragment())
-        }
-
-        btnToday.setOnClickListener {
-            replaceFragment(TodayFragment())
-        }
-
-        btnSearch.setOnClickListener {
-            replaceFragment(SearchFragment())
-        }
-
-        btnMypage.setOnClickListener {
-            replaceFragment(MypageFragment())
-        }
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainer, fragment)
-            .commit()
-    }
-}
+package com.example.plzhealthimport android.os.Bundleimport android.widget.LinearLayoutimport androidx.activity.enableEdgeToEdgeimport androidx.appcompat.app.AppCompatActivityimport androidx.core.view.ViewCompatimport androidx.core.view.WindowInsetsCompatimport androidx.fragment.app.Fragmentclass MainActivity : AppCompatActivity() {    override fun onCreate(savedInstanceState: Bundle?) {        super.onCreate(savedInstanceState)        enableEdgeToEdge()        setContentView(R.layout.activity_main)        replaceFragment(HealthPointFragment())        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)            insets        }        //하단바        val btnHealth = findViewById<LinearLayout>(R.id.btnHealth)        val btnToday = findViewById<LinearLayout>(R.id.btnToday)        val btnSearch = findViewById<LinearLayout>(R.id.btnSearch)        val btnMypage = findViewById<LinearLayout>(R.id.btnMypage)        btnHealth.setOnClickListener {            replaceFragment(HealthPointFragment())        }        btnToday.setOnClickListener {            replaceFragment(TodayFragment())        }        btnSearch.setOnClickListener {            replaceFragment(SearchFragment())        }        btnMypage.setOnClickListener {            replaceFragment(MypageFragment())        }    }    private fun replaceFragment(fragment: Fragment) {        supportFragmentManager.beginTransaction()            .replace(R.id.fragmentContainer, fragment)            .commit()    }}
